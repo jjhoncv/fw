f59b83afd7ec3c43ff1d39d8628f9dc425799d51
jhonnatan <jjhoncv@gmail.com>
2019-03-19 00:17:23 -0500

    feat(server): sections  api

---
+++ app/server/api/v1/sections.php
@@ -0,0 +1,57 @@
+<?php
+include '../inc.control_top.php';
+require_once _model_ . "User.php";
+require_once _model_ . "Users.php";
+require_once _utils_ . "headers.php";
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+switch ($method) {
+    case 'GET':
+        $get = (object) $_GET;
+        if (isset($get->_id)) {
+            $id = intval($_GET["id"]);
+            $section = new Section($id);
+            // $user->get();
+        } else {
+            $osections = new Sections();
+            $sections = $osections->getSections();
+
+            $code = 401;
+            $data = array(
+                "error" => array(
+                    "status" => $code,
+                    "description" => "Unauthorized",
+                ),
+            );
+
+            if (count($users) > 0) {
+                $code = 200;
+                foreach ($users as $user) {
+                    $list[] = array(
+                        "id" => $user->getId(),
+                        "role" => $user->getRole()->getName(),
+                        "name" => $user->getName(),
+                        "surname" => $user->getSurname(),
+                        "mail" => $user->getMail(),
+                        "photo" => $user->getPhoto(),
+                        "login" => $user->getLogin(),
+                        "reading" => $user->getReading(),
+                        "writing" => $user->getWriting(),
+                    );
+                }
+            }
+
+            $data = array(
+                "data" => $list,
+            );
+
+            http_response_code($code);
+            echo json_encode($data);
+        }
+        break;
+    default:
+        // Invalid Request Method
+        header("HTTP/1.0 405 Method Not Allowed");
+        break;
+}
---
+++ app/server/app/models/Section.php
@@ -0,0 +1,47 @@
+<?php
+require_once _model_ . 'Module.php';
+
+class Section
+{
+    private $_id, $_name, $_url, $_module;
+
+    public function __construct($id = 0)
+    {
+        $this->_id = $id;
+
+        if ($this->_id > 0) {
+
+            $sql = "SELECT * FROM sections WHERE id_section = '" . $this->_id . "'";
+            $section = Db::getInstance()->query($sql);
+
+            if ($section->count() > 0) {
+                $row = $section->result();
+                $this->_id = $row['id_section'];
+                $this->_module = new Module($row['id_module']);
+                $this->_name = $row['name_section'];
+                $this->_url = $row['url_section'];
+            }
+        }
+    }
+
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    public function getModule()
+    {
+        return $this->_module;
+    }
+
+    public function getUrl()
+    {
+        return $this->_url;
+    }
+
+}
